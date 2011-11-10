6e08f73050e667df48c093e92cdf3dca3c7f7b63
Balazs Varga <balo.software@gmail.com>
2011-11-10 20:24:15 +0100

    lista hozzáadás javítás - átírás modelles működésre

--- src/java/hu/sch/kurzuscsere/LessonPanel.java
+++ src/java/hu/sch/kurzuscsere/LessonPanel.java
@@ -7,7 +7,6 @@ package hu.sch.kurzuscsere;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
@@ -31,7 +30,7 @@ public final class LessonPanel extends Panel {
     String courseTo;
     String timeFrom;
     String timeTo;
-    Lesson newLesson;
+    Lesson lesson; //model object
     Form lessonForm;
 
     /*public Form getLessonForm() {
@@ -48,30 +47,17 @@ public final class LessonPanel extends Panel {
     protected void onInitialize() {
         super.onInitialize();
 
+        lesson = new Lesson();
+        final PropertyModel<Lesson> model = new PropertyModel(this, "lesson");
+
         final AppSession aps = (AppSession) getSession();
         final ArrayList<Lesson> lessonAdapter = ((WicketApplication) getApplication()).getLessons();
-        newLesson = new Lesson();
         lessonForm = new Form("lessonform");
         add(lessonForm);
 
-
-        /*<input type="text" wicket:id="lessonname"></input>
-        <input type="text" wicket:id="coursefrom"></input>
-        <input type="text" wicket:id="courseto"></input>
-        <input type="text" wicket:id="timefrom"></input>
-        <input type="text" wicket:id="timeto"></input>*/
-
-        PropertyModel<Lesson> lessonNameModel = new PropertyModel<Lesson>(newLesson, "name");
-        PropertyModel<Lesson> courseFromModel = new PropertyModel<Lesson>(newLesson, "courseFrom");
-        PropertyModel<Lesson> courseToModel = new PropertyModel<Lesson>(newLesson, "courseTo");
-
-        /*PropertyModel timeFromModel = new PropertyModel (this, "timeFrom");
-        PropertyModel timeToModel = new PropertyModel(this, "timeTo");
-         */
-
-        lessonForm.add(lsn = new TextField("lessonname", lessonNameModel));
-        lessonForm.add(cof = new TextField("coursefrom", courseFromModel));
-        lessonForm.add(cot = new TextField("courseto", courseToModel));
+        lessonForm.add(lsn = new TextField("lessonname", new PropertyModel(model, "name")));
+        lessonForm.add(cof = new TextField("coursefrom", new PropertyModel(model, "courseFrom")));
+        lessonForm.add(cot = new TextField("courseto", new PropertyModel(model, "courseTo")));
         lessonForm.add(tif = new TextField("timefrom", Model.of(dateFormat)));
         lessonForm.add(tit = new TextField("timeto", Model.of(dateFormat)));
         lessonForm.add(new Button("btnSub") {
@@ -85,12 +71,14 @@ public final class LessonPanel extends Panel {
                 String timeto = tit.getDefaultModelObjectAsString();
 
                 try {
+                    Lesson lesson = model.getObject();
+                    lesson.setTimeFrom(sdf.parse(timefrom));
+                    lesson.setTimeTo(sdf.parse(timeto));
 
-                    newLesson.setTimeFrom(sdf.parse(timefrom));
-                    newLesson.setTimeTo(sdf.parse(timeto));
+                    lessonAdapter.add(lesson);
+                    info(lesson.toString());
 
-                    lessonAdapter.add(newLesson);
-                    info(newLesson.toString());
+                    model.setObject(new Lesson());
 
                 } catch (ParseException ex) {
                     error("DATEFORMAT KOCSOG"); //Logger.getLogger(LessonPanel.class.getName()).log(Level.SEVERE, null, ex);
