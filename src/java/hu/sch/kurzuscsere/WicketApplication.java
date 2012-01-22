65d247ee9f0f9ebcfb4acd3d6dec370e962b8e11
Balazs Varga <balo.software@gmail.com>
2012-01-22 02:04:26 +0100

    * Futási mód olvasása JVM propertyből (#10) * ServerTimeFilter hozzáadása, hogy lássuk a renderelési időket

--- src/java/hu/sch/kurzuscsere/WicketApplication.java
+++ src/java/hu/sch/kurzuscsere/WicketApplication.java
@@ -1,8 +1,10 @@
 package hu.sch.kurzuscsere;
 
+import hu.sch.kurzuscsere.config.ConfigKeys;
 import hu.sch.kurzuscsere.config.LoggerSetup;
 import hu.sch.kurzuscsere.page.HomePage;
 import hu.sch.kurzuscsere.session.AppSession;
+import hu.sch.kurzuscsere.util.ServerTimerFilter;
 import org.apache.wicket.Request;
 import org.apache.wicket.Response;
 import org.apache.wicket.protocol.http.WebApplication;
@@ -28,8 +30,33 @@ public class WicketApplication extends WebApplication {
     }
 
     @Override
+    protected void init() {
+        super.init();
+
+        //getApplicationSettings().setPageExpiredErrorPage(PageExpiredError.class);
+        getPageSettings().setAutomaticMultiWindowSupport(false);
+
+        //Ha dev módban vagyunk, akkor hozzáteszünk egy új filtert, ami mutatja
+        //a render időket a log fájlban.
+        if (getConfigurationType().equals(DEVELOPMENT)) {
+            getRequestCycleSettings().addResponseFilter(new ServerTimerFilter());
+            log.info("Successfully enabled ServerTimerFilter");
+        }
+    }
+
+    @Override
     public Class getHomePage() {
         return HomePage.class;
+    }
+
+    @Override
+    public String getConfigurationType() {
+        String prop = System.getProperty(ConfigKeys.JVM_PROP_ENVIRONMENT, DEPLOYMENT);
+        if (prop.equals("DEV")) {
+            return DEVELOPMENT;
+        }
+
+        return DEPLOYMENT;
     }
 
     @Override
--- src/java/hu/sch/kurzuscsere/config/ConfigKeys.java
+++ src/java/hu/sch/kurzuscsere/config/ConfigKeys.java
@@ -8,6 +8,7 @@ public abstract class ConfigKeys {
 
     public static final String JDBC_RES = "jdbc/KurzusCsereDb";
     public static final String LOGBACK_CONF = "logback.xml";
+    public static final String JVM_PROP_ENVIRONMENT = "kurzuscsere.environment";
 
     private ConfigKeys() {
     }
