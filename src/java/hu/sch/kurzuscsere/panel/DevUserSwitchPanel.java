6a450956d427a35fd47908d6cb79093f76aa0690
Balazs Varga <balo.software@gmail.com>
2012-01-26 23:13:13 +0100

    Felhasználó-váltó és adatmódosító, kicsinyíthető fejlesztői panel (#11)

---
+++ src/java/hu/sch/kurzuscsere/panel/DevUserSwitchPanel.java
@@ -0,0 +1,73 @@
+package hu.sch.kurzuscsere.panel;
+
+import hu.sch.kurzuscsere.authz.DummyAuthorization;
+import hu.sch.kurzuscsere.domain.User;
+import hu.sch.kurzuscsere.logic.UserManager;
+import org.apache.wicket.Application;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.PropertyModel;
+
+/**
+ *
+ * @author balo
+ */
+public class DevUserSwitchPanel extends Panel {
+
+    public DevUserSwitchPanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        final WebMarkupContainer formContainer = new WebMarkupContainer("formContainer");
+        add(formContainer.setOutputMarkupId(true));
+
+        final Form changeDevUserFrm = new Form("changeDevUser") {
+
+            @Override
+            protected void onSubmit() {
+                super.onSubmit();
+
+                UserManager.getInstance().updateUserAttributes(getDevUser());
+            }
+        };
+
+        changeDevUserFrm.setVisible(false);
+        formContainer.add(changeDevUserFrm);
+
+        changeDevUserFrm.add(new TextField("devUserNick", new PropertyModel(getDevUser(),
+                "nick")));
+        changeDevUserFrm.add(new TextField("devUserName", new PropertyModel(getDevUser(),
+                "name")));
+        changeDevUserFrm.add(new TextField("devUserMail", new PropertyModel(getDevUser(),
+                "email")));
+
+        add(new AjaxLink("devPanelSwitcher") {
+
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+
+                changeDevUserFrm.setVisible(!changeDevUserFrm.isVisible());
+
+                target.addComponent(formContainer);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean isVisible() {
+        return getApplication().getConfigurationType().equals(Application.DEVELOPMENT);
+    }
+
+    private User getDevUser() {
+        return DummyAuthorization.actualTestUser;
+    }
+}
