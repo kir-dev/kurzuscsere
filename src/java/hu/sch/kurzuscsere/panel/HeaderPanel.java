20fcd4d8942199f3e8aa645ea4aaaf7da4795e66
Balazs Varga <balo.software@gmail.com>
2012-01-26 23:12:30 +0100

    Felhasználó-váltó és adatmódosító, kicsinyíthető fejlesztői panel

--- src/java/hu/sch/kurzuscsere/panel/HeaderPanel.java
+++ src/java/hu/sch/kurzuscsere/panel/HeaderPanel.java
@@ -1,10 +1,13 @@
 package hu.sch.kurzuscsere.panel;
 
+import hu.sch.kurzuscsere.authz.DummyAuthorization;
+import hu.sch.kurzuscsere.domain.User;
 import hu.sch.kurzuscsere.page.HomePage;
 import hu.sch.kurzuscsere.page.MyRequestsPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
 /**
  *
@@ -22,6 +25,10 @@ public class HeaderPanel extends Panel {
         super.onInitialize();
 
         add(new Label("pageTitle"));
+        add(new Label("loggedInUser",
+                new Model<User>(DummyAuthorization.actualTestUser)));
+
+        add(new DevUserSwitchPanel("devPanel"));
 
         add(new BookmarkablePageLink<HomePage>("homeMenuLink", HomePage.class));
         add(new BookmarkablePageLink<MyRequestsPage>("myreqMenuLink",
